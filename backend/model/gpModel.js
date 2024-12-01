c8bc8e4b4c1aca75cf7ebd1a800d3afc5e5c2750
Prince Mishra <pm618209@gmail.com>
2024-12-01 15:22:24 +0530

    ALL PHED API WORKING

--- backend/model/gpModel.js
+++ backend/model/gpModel.js
@@ -8,7 +8,11 @@ const { hashPassword, updateUpdatedAt } = require("../middleweres/Hashing.js");
 const GpSchema = new mongoose.Schema({
   name: { type: String, required: true },
   contact: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
+  email: {
+    type: String,
+    unique: true,  // This enforces unique values for the 'email' field
+    required: false, // Set to false if email is not mandatory
+},
   password: { type: String, required: true },
   lgdCode: { type: String, required: true },
   role: { type: String, default: "GP" },
